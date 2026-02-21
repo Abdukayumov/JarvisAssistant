23ab06a6b264fbe2f389d821068dc20c781ffc80
Abdukayumov <90976864+Abdukayumov@users.noreply.github.com>
2026-02-21 20:14:54 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+mkdir app
+mkdir -p app/src/main/java/com/jarvis
+mkdir -p app/src/main/res/layout
+touch settings.gradle
+touch build.gradle
+touch app/build.gradle
+touch app/src/main/AndroidManifest.xml
+touch app/src/main/java/com/jarvis/MainActivity.java
