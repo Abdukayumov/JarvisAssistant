aca45302039927b357d3a7a3b95d439c127da7f7
Abdukayumov <90976864+Abdukayumov@users.noreply.github.com>
2026-02-22 05:52:46 +0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-mkdir app
-mkdir -p app/src/main/java/com/jarvis
-mkdir -p app/src/main/res/layout
-touch settings.gradle
-touch build.gradle
-touch app/build.gradle
-touch app/src/main/AndroidManifest.xml
-touch app/src/main/java/com/jarvis/MainActivity.java
